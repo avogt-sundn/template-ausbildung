ca448fc8237f672ea691f3f11d39acfa6f328245
Dominik Böcker <dominik.boecker@sn-invent.de>
2023-03-29 14:20:23 +0200

    add esLint

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app/app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
