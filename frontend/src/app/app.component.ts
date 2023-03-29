05041691b8771e79996eadc8f54e5b7c2473ca9a
Dominik Böcker <dominik.boecker@sn-invent.de>
2023-03-29 13:51:43 +0200

    convert to standalone, remove demo code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'tod-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'tod-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: []
 })
 export class AppComponent {
-  title = 'ToDo-UI';
+  title = 'ToDo';
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,16 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+      provideRouter(appRoutes),
+    ]
+})
   .catch(err => console.error(err));
